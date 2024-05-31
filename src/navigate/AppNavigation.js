35ba73c7dee9b95a09cb1cf4e8cb6a18ccd43620
Medstili <codehub2023@gmail.com>
2024-05-31 22:58:36 +0100

    .

--- src/navigate/AppNavigation.js
+++ src/navigate/AppNavigation.js
@@ -1,11 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+
+// importing the screen tha we want to navigate to 
 import Login from "../screens/auth/Login";
 import SignUp from "../screens/auth/SignUp";
 import ForgotPassword from "../screens/auth/ForgotPassword";
-import Welcome from '../screens/Welcome'
+import Welcome from '../screens/Welcome';
+import Members from "../screens/Members"
+
+// importing the bottom bar  navigation 
 import BottomNavigation from "./BottomNavigation";
+import { primaryColor } from "../config/Config";
+
 
 const stack = createNativeStackNavigator();
 
@@ -13,13 +20,36 @@ export default function AppNavigation() {
   return (
     <NavigationContainer>
       <stack.Navigator 
-      initialRouteName="Welcome" 
+      //  making teh welcome screen be the first pages displayed to the user
+      initialRouteName="Home"
+      // hiding the header in this screens (welcome,login,sign up, forgot password)
       screenOptions={({ route }) => ({headerShown:false})}>
+
         <stack.Screen name={"Welcome"} component={Welcome} />
         <stack.Screen name={"Login"} component={Login} />
         <stack.Screen name={"Sign Up"} component={SignUp} />
         <stack.Screen name={"Forgot Password"} component={ForgotPassword} />
+        <stack.Screen 
+        options={{
+          headerShown: true,
+          title:"Members",
+          headerStyle: {
+            backgroundColor: primaryColor,
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerBackTitle: 'Back',
+
+        }}
+        name={"Members"} component={Members} />
+        
+      {/* merging the bottomNavigation with home screen  */}
         <stack.Screen name={"Home"} component={BottomNavigation} />
+
+
+
       </stack.Navigator>
     </NavigationContainer>
   );
