2e49eb3f37cb984fa3069accefdda71ae482a757
leveeeera <ajbounrachid06@gmail.com>
2024-05-31 20:58:38 +0000

    notification changes

--- src/screens/Notifications.js
+++ src/screens/Notifications.js
@@ -1,99 +1,124 @@
 import *  as React from "react";
-import {View, Text, StyleSheet,Image,} from "react-native";
+import {View, Text, StyleSheet,Image, FlatList, SafeAreaView, TouchableOpacity} from "react-native";
 import { naturalColor, primaryColor, secondaryColor } from "../config/Config";
+
+import moment from 'moment'
+
 
 import { Entypo } from '@expo/vector-icons';
 
+// CREATE DATA FOR NOTIFICATION
+notificationData = [
 
+    {
+        id:1,
+        userImage: require('../../images/PostImg/profil2.jpg'),
+        notifContentText:'Mohammed Stili react to your psot: "salam 3afakom chkon chaf had siyid ...."',
+        timestamp: new Date(),
+    },
+    {
+        id:2,
+        userImage: require('../../images/PostImg/profil1.jpg'),
+        notifContentText:'Mohammed Stili react to your psot: "salam 3afakom chkon chaf had siyid ...."',
+        timestamp: new Date(),
+    },
+    {
+        id:3,
+        userImage: require('../../images/PostImg/profil5.jpg'),
+        notifContentText:'Mohammed Stili react to your psot: "salam 3afakom chkon chaf had siyid ...."',
+        timestamp: new Date(),
+    },
+    {
+        id:4,
+        userImage: require('../../images/PostImg/profil3.jpg'),
+        notifContentText:'Mohammed Stili react to your psot: salam 3afakom chkon chaf had siyid ....',
+        timestamp: new Date(),
+    },
+    {
+        id:5,
+        userImage: require('../../images/PostImg/profil9.jpg'),
+        notifContentText:'Mohammed Stili react to your psot: "salam 3afakom chkon chaf yaha mskin da3 lina  ...."',
+        timestamp: new Date(),
+    },
+    {
+        id:6,
+        userImage: require('../../images/PostImg/profil6.jpg'),
+        notifContentText:'Mohammed Stili react to your psot: "salam 3afakom chkon chaf had siyid ...."',
+        timestamp: new Date(),
+    },
+    {
+        id:7,
+        userImage: require('../../images/PostImg/profil4.jpg'),
+        notifContentText:'Mohammed Stili react to your psot: "salam 3afakom chkon chaf had siyid ...."',
+        timestamp: new Date(),
+    },
+    {
+        id:8,
+        userImage: require('../../images/PostImg/profil7.jpg'),
+        notifContentText:'Mohammed Stili react to your psot: "salam 3afakom chkon chaf had siyid ...."',
+        timestamp: new Date(),
+    },
+    {
+        id:9,
+        userImage: require('../../images/PostImg/profil1.jpg'),
+        notifContentText:'Mohammed Stili react to your psot: "salam 3afakom chkon chaf had siyid ...."',
+        timestamp: new Date(),
+    },
+
+]
 
 export default function NoatificationsPage() {
-    return(
-        
-        <View style={styles.container}>
-            <View style={styles.notifBox}>
-                <Image source={require("../../images/PostImg/profil1.jpg")} style={styles.userProfile}/>
-                <View style={styles.textBox}>
-                    <Text style={styles.notifMessage} >
-                        Mohammed Stili react to your psot: "salam 3afakom chkon chaf had siyid ...."
-                    </Text>
-                    <Text style={styles.notifTimer}>45min</Text>
-                </View>
-                <View style={styles.notifSetteing}>
-                    <Entypo name="dots-three-horizontal" size={20} color={primaryColor} />
-                </View>
-            </View>
-            <View style={styles.notifBox}>
-                <Image source={require("../../images/PostImg/profil7.jpg")} style={styles.userProfile}/>
-                <View style={styles.textBox}>
-                    <Text style={styles.notifMessage} >
-                        Mohammed Stili react to your psot: "salam 3afakom chkon chaf had siyid ...."
-                    </Text>
-                    <Text style={styles.notifTimer}>1h</Text>
-                </View>
-                <View style={styles.notifSetteing}>
-                    <Entypo name="dots-three-horizontal" size={20} color={primaryColor} />
-                </View>
-            </View>
 
+    const OnNotification = ({ item }) => (
+        <View style={styles.item}>
             <View style={styles.notifBox}>
-                <Image source={require("../../images/PostImg/profil3.jpg")} style={styles.userProfile}/>
-                <View style={styles.textBox}>
-                    <Text style={styles.notifMessage} >
-                        Mohammed Stili react to your psot: "salam 3afakom chkon chaf had siyid ...."
-                    </Text>
-                    <Text style={styles.notifTimer}>8h</Text>
-                </View>
-                <View style={styles.notifSetteing}>
-                    <Entypo name="dots-three-horizontal" size={20} color={primaryColor} />
-                </View>
-            </View>
 
-            <View style={styles.notifBox}>
-                <Image source={require("../../images/PostImg/profil6.jpg")} style={styles.userProfile}/>
+                    <Image source={item.userImage} style={styles.userAvatra}/>
                 <View style={styles.textBox}>
-                    <Text style={styles.notifMessage} >
-                        Mohammed Stili react to your psot: "salam 3afakom chkon chaf yaha mskin da3 lina  ...."
-                    </Text>
-                    <Text style={styles.notifTimer}>22h</Text>
+                    <TouchableOpacity>
+                        <Text style={styles.notifMessage} >{item.notifContentText}</Text>
+                        <Text style={styles.notifTimer}>{moment(item.timestamp).fromNow()}</Text>
+                    </TouchableOpacity>
                 </View>
-                <View style={styles.notifSetteing}>
-                    <Entypo name="dots-three-horizontal" size={20} color={primaryColor} />
-                </View>
-            </View>
 
-            <View style={styles.notifBox}>
-                <Image source={require("../../images/PostImg/profil9.jpg")} style={styles.userProfile}/>
-                <View style={styles.textBox}>
-                    <Text style={styles.notifMessage} >
-                        Aicha dwiba react to your psot: "salam 3afakom chkon chaf had siyid ...."
-                    </Text>
-                    <Text style={styles.notifTimer}>1D</Text>
-                </View>
                 <View style={styles.notifSetteing}>
-                    <Entypo name="dots-three-horizontal" size={20} color={primaryColor} />
+                    <TouchableOpacity>
+                        <Entypo name="dots-three-horizontal" size={16} color={primaryColor} />
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
+
+    )
+
+    return(
+        <SafeAreaView>
+        <FlatList 
+            data = {notificationData}
+            renderItem={OnNotification}
+        />
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    item: {
         flex:1,
         backgroundColor:'white',
-        gap:10
+        gap:18
     },
     notifBox: {
         padding:10,
         flexDirection:'row',
-        gap:10,
-        alignContent:'center',
+        gap:8,
+        justifyContent:'center',
+        alignItems:'center',
         backgroundColor:naturalColor
     },
-    userProfile: {
-        width:60,
+    userAvatra: {
+        width: 60,
         height:60,
-        borderRadius:40,
+        borderRadius:30
     },
     textBox: {
         flex:1,
@@ -101,6 +126,13 @@ const styles = StyleSheet.create({
     },
     notifMessage: {
         fontSize:14,
-        bottom:4,
     },
+    notifSetteing: {
+        alignSelf:'flex-start',
+        right:6,
+    },
+    notifTimer: {
+        top:10,
+        color:primaryColor
+    }
 })
