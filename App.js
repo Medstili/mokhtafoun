daec77510ce1b61289436594f61018055d38a10a
Medstili <codehub2023@gmail.com>
2024-06-23 18:20:16 +0100

    adding drawer navigation and memberProfile and search bar in home page

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import AppNavigation from "./src/navigate/AppNavigation";
+import DrawerNavigation from "./src/navigate/DrawerNavigation";
+
 
 
 export default function App() {
   return(
+    <NavigationContainer>
       <AppNavigation/> 
+      {/* <DrawerNavigation/> */}
+    </NavigationContainer>
     );
 };
